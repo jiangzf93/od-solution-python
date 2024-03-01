2147b77d8c217b61441bb3910e1d6f2471329e5f
jiangzf93 <jiangdm93@hotmail.com>
2024-03-01 16:22:40 +0800

    202403011622

--- C_14_horse/solution.py
+++ C_14_horse/solution.py
@@ -1,9 +1,51 @@
 class Solution(object):
+    def __init__(self):
+        self.dirctions = [(1,2),(1,-2),(-1,2),(-1,-2),(2,1),(2,-1),(-2,1),(-2,-1)]
+
+    def bfs(self, row, col, step):
+        stepMatrix = [[-1 for _ in range(self.cols)] for _ in range(self.rows)]
+        currStep = 0
+        points = [(row, col)]
+        while currStep <= step:
+            buffer = []
+            for point in points:
+                r, c= point
+                if stepMatrix[r][c] == -1:
+                    stepMatrix[r][c] = currStep
+                for d in self.dirctions:
+                    newR = r + d[0]
+                    newC = c + d[1]
+                    if newR >= 0 and newR < self.rows and newC >= 0 and newC < self.cols and stepMatrix[newR][newC] == -1:
+                        buffer.append((newR, newC))
+            points = buffer
+            currStep += 1
+        for r in range(self.rows):
+            for c in range(self.cols):
+                if stepMatrix[r][c] == -1 or self.resultMatrix[r][c] == -1:
+                    self.resultMatrix[r][c] = -1
+                else:
+                    self.resultMatrix[r][c] += stepMatrix[r][c]
+
+
+
     def solve(self, inputStr):
         inputLines = inputStr.split('\n')
         inputDim = inputLines[0].split()
-        rows = int(inputDim[0])
-        cols = int(inputDim[1])
-        matrix = [[int(c) for c in r[:cols]] for r in inputLines[1:rows+1]]
-        
-        return False
+        self.rows = int(inputDim[0])
+        self.cols = int(inputDim[1])
+        matrix = [['.' if c == '.' else int(c) for c in r.split()[:self.cols]] for r in inputLines[1:self.rows+1]]
+        self.resultMatrix = [[0 for _ in range(self.cols)] for _ in range(self.rows)]
+        for r in range(self.rows):
+            for c in range(self.cols):
+                if matrix[r][c] == '.':
+                    continue
+                else:
+                    self.bfs(r, c, matrix[r][c])
+
+        result = float('inf')
+        for r in range(self.rows):
+            for c in range(self.cols):
+                if self.resultMatrix[r][c] != -1:
+                    result = min(result, self.resultMatrix[r][c])
+                
+        return result
--- C_7_5G/solution.py
+++ C_7_5G/solution.py
@@ -1,8 +1,9 @@
 class Solution(object):
-    def getRoot(self, node):
-        if (self.roots[node] == node):
-            return node
-        
+
+    def mergeRoots(self, fromRoot, toRoot):
+        for r in range(len(self.roots)):
+            if self.roots[r] == fromRoot:
+                self.roots[r] = toRoot
 
     def solve(self, inptStr):
         inputLines = inptStr.split('\n')
@@ -10,17 +11,27 @@ class Solution(object):
         numPairs = int(inputLines[1])
         pairs = [[int(i) for i in p.split()] for p in inputLines[2:numPairs+2]]
         self.roots = [i for i in range(numSites+1)]
-        connParts = 0
         for pair in pairs:
             site1 = pair[0]
             site2 = pair[1]
             connected = pair[3]
-            if roots[site1] != roots[site2] and connected:
-                roots[site1] = roots[site2]
-                connParts += 1
+            if pair[3]:
+                if self.roots[site1] != self.roots[site2] and connected:
+                    self.mergeRoots(self.roots[site1], self.roots[site2])
+
+        pairs = sorted(pairs, key = lambda x: x[2])
+        cost = 0
+        connected = False
         for pair in pairs:
+            if len(list(set(self.roots[1:]))) == 1:
+                connected = True
+                break
             if pair[3] == 1:
                 continue
+            else:
+                self.mergeRoots(self.roots[pair[0]], self.roots[pair[1]])
+                cost += pair[2]
+        if len(list(set(self.roots[1:]))) == 1:
+            connected = True
 
-
-        return 1
+        return cost if connected else -1
